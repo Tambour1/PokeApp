9dbf58308d0ff4588ea98b6f373c295c87ac8748
Victor Georges <victorgeorges54@gmail.com>
2025-01-21 00:56:47 +0100

    💄 Ajout d'une font et les icons des types de pokemons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pokemon': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
